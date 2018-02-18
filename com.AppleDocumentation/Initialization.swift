6192a348881edfd304eed589e1c94ab0c01d7cc9
Jamie Chu <huanjamierose@gmail.com>
2018-02-18 11:04:09 -0500

    init continue

--- com.AppleDocumentation/Initialization.swift
+++ com.AppleDocumentation/Initialization.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+//Helpers for Initialization
+class InitClass{
+
+    var temperature: Double
+    var status: String {
+        switch self.temperature{
+        case let x where x < 0:
+            return "below freezing"
+        case 1..<30:
+            return "Cold"
+        case 30..<70:
+            return "Normal"
+        case 70..<100:
+            return "Hot"
+        default:
+            return "Hell"
+        }
+    }
+    
+    var name: String?
+    
+    convenience init(){
+        self.init(temperature:40)
+    }
+    
+    init(temperature: Double) {
+        self.temperature = temperature
+    }
+
+}
+
+class InitClassDefault{
+    var temperature = 32.1
+}
+
+struct InitStruct{
+    let temperature: Double
+}
+
+struct InitStructDefault{
+    let temperature = 32.1
+    let param2: Double
+}
+
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+    init(white: Double) {
+        red   = white
+        green = white
+        blue  = white
+    }
+}
+
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+
--- com.AppleDocumentation/ViewController.swift
+++ com.AppleDocumentation/ViewController.swift
@@ -12,14 +12,53 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        initDemo1()
+        initDemo2()
     }
 
 
 }
 
+typealias Initialization = ViewController
+extension Initialization{
+/*
+    KEY POINTS:
+    Initialization is the process of preparing an instance of a class, structure, or enum for use
+    Classes and structures must set all of their stored properties to a appropriate value
+     
+*/
+    func initDemo1(){
+        //class convenience init with no parameters
+        let a1 = InitClass()
+        print("a1's temperature is \(a1.temperature)")
+        print("optionals are not required in init: a.name's default is \(String(describing: a1.name))")
+
+        //second class init
+        let a2 = InitClass(temperature: 22)
+        print("a2's temperature is \(a2.temperature)")
+        print("a2's status is \(a2.status)")
+        
+        //swift gives structures a default memberwise init if they do not define their own
+        let b1 = InitStruct(temperature: 11)
+        print("b's temperature is \(b1.temperature)")
+        
+        //you can provide a default value (they would not be included in the memberwise init)
+        let b2 = InitStructDefault(param2: 33.0)
+        print("temperature is \(b2.temperature) and is not included in the memberwise initializer ")
+        
+    }
+    
+    
+    func initDemo2(){
+        //init may vary as long as it provides values for all stored non-optional properties
+        let magenta = Color(red: 1, green: 1, blue: 1)
+        let halfGray = Color(white: 0.5)
+
+        print("\(magenta) \(halfGray)")
+        
+        //Color has constants as its properties. It may be set during initialization but may not be modified afterwards.
+        
+        
+    }
+    
+}
