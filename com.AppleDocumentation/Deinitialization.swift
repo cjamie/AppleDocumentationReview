074d44e49caf50f89d786569e892aae215249cca
Jamie Chu <huanjamierose@gmail.com>
2018-02-19 20:56:14 -0500

    deinit example finished

--- com.AppleDocumentation/Deinitialization.swift
+++ com.AppleDocumentation/Deinitialization.swift
@@ -19,3 +19,16 @@ class Bank {
         coinsInBank += coins
     }
 }
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
--- com.AppleDocumentation/VC+Extensions/VC+Deinitialization.swift
+++ com.AppleDocumentation/VC+Extensions/VC+Deinitialization.swift
@@ -10,12 +10,38 @@ import Foundation
 
 /*
  A deinitializer is called immediately before a CLASS instance is deallocated. It is not available to structs or enums - only classes.
+ 
+ the deinit{} function is called when no other properties or variables are referencing it.
 
  */
 
 typealias Deinitialization = ViewController
 extension Deinitialization{
     func deinitDemo(){
+        
+        var temp = Player(coins: 200)//bank just distributed 200 coins via init
+        print(Bank.coinsInBank)  // 10000 - 200 = 9800
+        
+        temp.win(coins: 300) //bank distributes another 300 coins
+        print(Bank.coinsInBank) //9800-300 = 9500
+        
+        temp = Player(coins: 0) //9500-> 10000 - 0 = 10000
+        print(Bank.coinsInBank) //previous player no longer referenced so de-init is called. new player takes no coins so 10000
+
+        //-------------------
+        
+        var temp1:Player? = Player(coins: 100) //10000 - 100 = 9900
+        var temp2:Player? = Player(coins: 250) // 9900 - 250 = 9650
+        
+        _ = Player(coins: 300) //player has been initialized but deinit has also been called because there are no references to it
+        //9650 - 0 = 9650
+        print(Bank.coinsInBank)
+        print("\(temp1?.coinsInPurse ?? -9001) \(temp2?.coinsInPurse ?? -9001)")
+
+        temp1 = nil //9650 + 100 = 9750
+        temp2 = nil //9750 + 250 = 10000
+        print(Bank.coinsInBank)
+        
         
     }
 }
